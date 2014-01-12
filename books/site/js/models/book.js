d6be55d8d4d96ad3a58a64a535b27cf11c23cda9
Daniel Hanold <danny@danielhanold.com>
2014-01-12 13:27:14 -0500

    Ensure ID from server is matched with id in backbone model. Allows deleting of models

--- books/site/js/models/book.js
+++ books/site/js/models/book.js
@@ -11,6 +11,15 @@ app.Book = Backbone.Model.extend({
     title: 'No title',
     author: 'Unknown',
     releaseDate: 'Unknown',
-    keywords: 'None'
+    keywords: 'None',
+  },
+
+  // Parse function required when new models are
+  // saved locally. The server stores the id of
+  // a book as "_id", we need to account for that
+  // here to keep everything in sync.
+  parse: function(response) {
+    response.id = response._id;
+    return response;
   }
 });
